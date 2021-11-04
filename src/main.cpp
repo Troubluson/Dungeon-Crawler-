52ff26b829e7c46f8bfa9da1891740374b7c9e25
Oskar Björkgren <oskar.bjorkgren@aalto.fi>
2021-11-04 16:01:53 +0200

    Implemented basic version of Game Class and Player class

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,17 @@
 #include "Platform/Platform.hpp"
+#include "game.hpp"
 
 int main()
 {
-  util::Platform platform;
 
-#if defined(_DEBUG)
-  std::cout << "Hello World!" << std::endl;
-#endif
+  Game dungeonCrawler;
 
-  sf::RenderWindow window;
-  // in Windows at least, this must be called before creating the window
-  float screenScalingFactor =
-      platform.getScreenScalingFactor(window.getSystemHandle());
-  // Use the screenScalingFactor
-  window.create(
-      sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor),
-      "SFML works!");
-  platform.setIcon(window.getSystemHandle());
+  // Game loop
 
-  sf::CircleShape shape(window.getSize().x / 2);
-  shape.setFillColor(sf::Color::White);
-
-  sf::Texture shapeTexture;
-  shapeTexture.loadFromFile("content/sfml.png");
-  shape.setTexture(&shapeTexture);
-  sf::Event event;
-
-  while (window.isOpen())
+  while (dungeonCrawler.Running())
   {
-    while (window.pollEvent(event))
-    {
-      if (event.type == sf::Event::Closed)
-        window.close();
-    }
-
-    window.clear();
-    window.draw(shape);
-    window.display();
+    dungeonCrawler.UpdateGame();
+    dungeonCrawler.RenderGame();
   }
 
   return 0;
