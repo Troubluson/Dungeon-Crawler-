dc945dc3bae9df49cef0356851251cb38f090f99
Jonas Edström <jonas.edstrom@aalto.fi>
2021-11-08 15:14:06 +0200

    Added simple monster class

---
+++ src/monster.cpp
@@ -0,0 +1,3 @@
+#include "monster.hpp"
+
+Monster::Monster() {}
---
+++ src/monster.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef _MONSTER_CLASS_
+#define _MONSTER_CLASS_
+
+#include "character.hpp"
+
+class Monster : public Character
+{
+public:
+	Monster();
+};
+
+#endif
