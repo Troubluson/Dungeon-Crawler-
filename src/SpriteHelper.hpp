aa351db3cdb5152affec724b7d74c957b3bc8f98
Henrik <henrik.niskanen.aalto.fi>
2021-11-22 21:03:52 +0200

    Added generic functions to spritehelper to reduce visual complexity

--- src/SpriteHelper.hpp
+++ src/SpriteHelper.hpp
@@ -8,6 +8,8 @@ private:
 public:
     SpriteHelper(/* args */) {};
     void CreateSpriteFrom(const std::string& spriteFile, sf::Vector2f dimensions, sf::Sprite& sprite, sf::Texture& texture);
+    void SetScale(sf::Vector2f wantedDimension, sf::Sprite& sprite);
+    void RotateSprite(sf::Vector2f directionOfRotation, sf::Sprite& sprite);
 };
 
 #endif
