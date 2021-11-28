28c22f06ecc3fb74a9be32cf03bb0d7340d4c528
mikael <mikael.virkkunen@aalto.fi>
2021-11-28 18:35:00 +0200

    Fixed Dividing by zero problem

--- src/Actors/SearchingMonster.cpp
+++ src/Actors/SearchingMonster.cpp
@@ -16,7 +16,10 @@ void SearchingMonster::Move(float dt)
     sf::Vector2f playerpos = GetPlayer().GetSpriteCenter();
     sf::Vector2f distanceVec = playerpos - GetSpriteCenter();
     float distance = std::sqrt(distanceVec.x * distanceVec.x + distanceVec.y * distanceVec.y);
-    sf::Vector2f velocityVec = distanceVec / distance;
+    sf::Vector2f velocityVec = sf::Vector2f(0, 0);
+    if (distance != 0.0f) {
+        velocityVec = distanceVec / distance;
+    }
     MoveRight(dt * velocityVec.x * 0.3);
     MoveDown(dt * velocityVec.y * 0.3);
 }
